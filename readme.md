fa028b6459f49346a2407b63b0db3541d76812fc
ROW <axlrower@gmail.com>
2025-03-07 22:43:23 -0300

    arreglando precios

--- readme.md
+++
@@ -1,1 +0,0 @@
-# https://github.com/Row-Diaz/Iinmobiliaria.git
--- scriptVenta.js
+++ scriptVenta.js
@@ -1,55 +1,55 @@
 // VENTAS
-    
+
 const propiedadesVenta = [
-    {
-      id: 3,
-      nombre: "Apartamento de lujo en zona exclusiva",
-      src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
-      descripcion:
-        "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
-      ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
-      habitaciones: 4,
-      baños: 4,
-      costo: 5.0,
-      smoke: false,
-      pets: false,
-      enVenta: true,
-    },
-    {
-      id: 4,
-      nombre: "Apartamento acogedor en la montaña",
-      src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
-      descripcion:
-        "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas.",
-      ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
-      habitaciones: 2,
-      baños: 1,
-      costo: 1.2,
-      smoke: true,
-      pets: true,
-      enVenta: true,
-    },
-    {
-      id: 5,
-      nombre: "Penthouse de lujo con terraza panorámica",
-      src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
-      descripcion:
-        "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares.",
-      ubicacion: "Skyline Avenue, Skyview City, CA 56789",
-      habitaciones: 3,
-      baños: 3,
-      costo: 4.5,
-      smoke: false,
-      pets: true,
-      enVenta: true,
-    },
-  ];
+  {
+    id: 3,
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 4,
+    baños: 4,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+    enVenta: true,
+  },
+  {
+    id: 4,
+    nombre: "Apartamento acogedor en la montaña",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas.",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: 2,
+    baños: 1,
+    costo: 1200,
+    smoke: true,
+    pets: true,
+    enVenta: true,
+  },
+  {
+    id: 5,
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares.",
+    ubicacion: "Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: 3,
+    baños: 3,
+    costo: 4500,
+    smoke: false,
+    pets: true,
+    enVenta: true,
+  },
+];
 
-  const ventaSection = document.getElementById("venta");
+const ventaSection = document.getElementById("venta");
 
-  for (let buy of propiedadesVenta) {
-    if (buy.enVenta) {
-      const propiedadTemplateString = `
+for (let buy of propiedadesVenta) {
+  if (buy.enVenta) {
+    const propiedadTemplateString = `
     <section id="alquiler1" class="mb-5">
     <div class="row">
     <div class="col-md-4 mb-4">
@@ -79,9 +79,7 @@ const propiedadesVenta = [
                         <p><i class="fas fa-dollar-sign"></i>${buy.costo}</p>
                         <p class="text-danger">
                         <i class="fas fa-smoking-ban"></i>${
-                          buy.smoke
-                            ? "Permitido fumar"
-                            : "No se permite fumar"
+                          buy.smoke ? "Permitido fumar" : "No se permite fumar"
                         } 
                         </p>
                         <p class="text-success">
@@ -98,7 +96,6 @@ const propiedadesVenta = [
                 
                   </section>
                     `;
-      ventaSection.innerHTML += propiedadTemplateString;
-    }
+    ventaSection.innerHTML += propiedadTemplateString;
   }
-
+}
